859eb2d89446cab7d27ae3057ccccfe7c13251e1
Vien Tang <vientang@gmail.com>
2016-12-09 02:32:52 -0800

    Add test that describes the vote function, implement the vote function

--- src/core.js
+++ src/core.js
@@ -7,7 +7,15 @@ export function setEntries(state, entries) {
 export function next(state) {
 	const entries = state.get('entries');
 	return state.merge({
-		vote: new Map({ pair: entries.take(2) }),
+		vote: Map({ pair: entries.take(2) }),
 		entries: entries.skip(2)
 	});
+}
+
+export function vote(state, entry) {
+	return state.updateIn(
+		['vote', 'tally', entry],
+		0,
+		tally => tally++
+	);
 }
