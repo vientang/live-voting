8993f8217283073373825bf3bb07e255f295bb45
Vien Tang <vientang@gmail.com>
2016-12-09 17:29:23 -0800

    Add a test for next function that should move winner back into entries collection, and implentthe function

--- test/core_spec.js
+++ test/core_spec.js
@@ -31,7 +31,49 @@ describe("application logic", () => {
 				entries: List.of('Sunshine')
 			}));
 		});
+		
+		it('puts winner of the current vote back into entries', () => {
+			const state = Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
+					tally: Map({
+						'Trainspotting': 4,
+						'28 Days Later': 2
+					})
+				}),
+				entries: List.of('Sunshine', 'Millions', '127 Hours')
+			});
+			const nextState = next(state);
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Sunshine', 'Millions'),
+				}),
+				entries: List.of('127 Hours', 'Trainspotting')
+			}));
+		});
+
+		it('puts both titles back in entries if vote is a tie', () => {
+			const state = Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
+					tally: Map({
+						'Trainspotting': 4,
+						'28 Days Later': 4
+					})
+				}),
+				entries: List.of('Sunshine', 'Millions')
+			});
+			const nextState = next(state);
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Sunshine', 'Millions')
+				}),
+				entries: List.of('127 Hours', 'Trainspotting', '28 Days Later')
+			}));
+		});
+
 	});
+
 
 	describe("vote", () => {
 
